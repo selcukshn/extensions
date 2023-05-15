5ba0923cad50f7ba1c396281702d626b416947ff
selcuksahin <muselcuksahin@gmail.com>
2023-05-15 18:36:27 +0300

    turkish to english fix

--- Extensions/StringExtension.cs
+++ Extensions/StringExtension.cs
@@ -1,60 +1,32 @@
+using System.Globalization;
 using System.Text.RegularExpressions;
 
 namespace Console.Extensions
 {
     public static class StringExtension
     {
-        private static char[] TurkishChars = new char[] { 'ç', 'Ç', 'ğ', 'Ğ', 'ı', 'İ', 'ö', 'Ö', 'ş', 'Ş', 'ü', 'Ü' };
         private static Dictionary<char, char> TrToEng = new Dictionary<char, char>{
             {'ç','c'},{'Ç','C'},{'ğ','g'},{'Ğ','G'},{'ı','i'},{'İ','I'},{'ö','o'},{'Ö','O'},{'ş','s'},{'Ş','S'},{'ü','u'},{'Ü','U'}
         };
-        public static string TurkishToEnglish(this String str, bool toLower = true)
-        {
-            foreach (var c in TrToEng)
-            {
-                str = str.Replace(c.Key, c.Value);
-            }
-            if (toLower)
-                return str.ToLower();
-            return str;
-        }
-        private static string ReplaceSpecialChars(this String str, string replaceChar, string pattern)
-        {
-            return new Regex(pattern).Replace(str, replaceChar);
-        }
-        public static string ReplaceSpecialChars(this String str, string replaceChar)
-        {
-            return ReplaceSpecialChars(str, replaceChar, @"[^a-zA-Z0-9\- ]");
-        }
-        public static string ReplaceSpecialChars(this String str, bool removeSpace, string replaceChar)
-        {
-            return ReplaceSpecialChars(str, replaceChar, removeSpace ? @"[^a-zA-Z0-9\-]" : @"[^a-zA-Z0-9\- ]");
-        }
-        public static string RemoveSpecialChars(this String str)
-        {
-            return new Regex(@"[^a-zA-Z0-9\-_ ]").Replace(str, "");
-        }
+        public static string TurkishToEnglish(this String str, bool toLower = true) =>
+            toLower ?
+            string.Concat(str.Select(c => TrToEng.ContainsKey(c) ? TrToEng[c] : c)).ToLower(CultureInfo.GetCultureInfoByIetfLanguageTag("en")) :
+            string.Concat(str.Select(c => TrToEng.ContainsKey(c) ? TrToEng[c] : c));
+
+        public static string ReplaceSpecialChars(this String str, string replaceChar) => ReplaceSpecialChars(str, replaceChar, @"[^a-zA-Z0-9\- ]");
+        public static string ReplaceSpecialChars(this String str, bool removeSpace, string replaceChar) => ReplaceSpecialChars(str, replaceChar, removeSpace ? @"[^a-zA-Z0-9\-]" : @"[^a-zA-Z0-9\- ]");
+        private static string ReplaceSpecialChars(this String str, string replaceChar, string pattern) => new Regex(pattern).Replace(str, replaceChar);
+
+        public static string RemoveSpecialChars(this String str) => new Regex(@"[^a-zA-Z0-9\-_ ]").Replace(str, string.Empty);
         public static string RemoveSpecialChars(this String str, params char[] exclude)
         {
-            string patternExt = string.Empty;
-            foreach (var c in exclude)
-            {
-                patternExt += $"\\{c}";
-            }
-            return new Regex(@$"[^a-zA-Z0-9\-_{patternExt} ]").Replace(str, "");
-        }
-        public static string ReplaceSpaces(this String str, char replaceChar)
-        {
-            return str.Trim().Replace(' ', replaceChar);
-        }
-        public static string RemoveSpaces(this String str)
-        {
-            return str.Trim().Replace(" ", string.Empty);
+            string patternExt = string.Concat(exclude.Select(c => $"\\{c}"));
+            return new Regex(@$"[^a-zA-Z0-9\-_{patternExt} ]").Replace(str, string.Empty);
         }
 
-        public static string ToUrl(this String str)
-        {
-            return str.TurkishToEnglish().ReplaceSpaces('-').RemoveSpecialChars();
-        }
+        public static string RemoveSpaces(this String str) => str.Trim().Replace(" ", string.Empty);
+        public static string ReplaceSpaces(this String str, char replaceChar) => str.Trim().Replace(' ', replaceChar);
+
+        public static string ToUrl(this String str) => str.TurkishToEnglish().ReplaceSpaces('-').RemoveSpecialChars();
     }
 }
